0109efdbd77839ce03c1412d67e603b946de43a0
Balazs Nadasdi <balazs.nadasdi@cheppers.com>
2019-12-19 19:33:33 +0100

    d17p1

---
+++ src/Day17/Part1.hs
@@ -0,0 +1,15 @@
+module Day17.Part1
+  ( solve
+  ) where
+
+import Day17.Lib
+import Intcode
+
+solve :: String -> String
+solve "No Input" = "No Input Defined!"
+solve input = show $ sum $ map (uncurry (*)) intersections
+  where
+    tape = parse input
+    machine = newMachine tape
+    machine' = executeMachine machine
+    intersections = findIntersections $ machineScreen machine'
---
+++ src/Day17/Part2.hs
@@ -0,0 +1,9 @@
+module Day17.Part2
+  (solve
+  ) where
+
+import Day17.Lib
+
+solve :: String -> String
+solve "No Input" = "No Input Defined!"
+solve input = "Day#17::Part2"
