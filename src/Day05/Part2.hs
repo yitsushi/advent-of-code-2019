d1a5a472b877d87151769fdd1f8ff3c598a3e30b
Balazs Nadasdi <yitsushi@protonmail.ch>
2019-12-29 23:31:07 +0100

    migrate day05 to IntcodeMachine

--- src/Day05/Part2.hs
+++ src/Day05/Part2.hs
@@ -2,10 +2,8 @@ module Day05.Part2
   ( solve
   ) where
 
-import Intcode
+import IntcodeMachine
 
 solve :: String -> String
 solve "No Input" = "No Input Defined!"
-solve input = (show . last . getOutput . execute) (Computer tape [5] 0 [] 0)
-  where
-    tape = parse input
+solve input = (show . last . output . boot) (loadComputer input [5])
