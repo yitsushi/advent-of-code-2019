e28b9a6e20070e8481fd16b2500718c66c256cc1
Efertone <efertone@pm.me>
2020-05-03 08:14:16 +0200

    a bit more brittany

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,27 +1,25 @@
-import Criterion.Main
+import           Criterion.Main
 
 import qualified Data.List.Split
-import qualified IntcodeMachine as IM
+import qualified IntcodeMachine                as IM
 import qualified Lib
 
 tape :: String
 tape =
   "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99"
 
-main =
-  defaultMain
-    [ bgroup
-        "lpad"
-        [ bench "50" $ whnf Lib.lpad (50, '0', "1")
-        , bench "100" $ whnf Lib.lpad (100, '0', "1")
-        ]
-    , bgroup
-        "splitOn"
-        [ bench "Lib" $ nf (Lib.splitOn ' ') (unwords $ replicate 50 "a")
-        , bench "Data.List.Split" $
-          nf (Data.List.Split.splitOn " ") (unwords $ replicate 50 "a")
-        ]
-    , bgroup
-        "Intcode Machine"
-        [bench "<> 8" $ whnf IM.boot (IM.loadComputer tape [10])]
+main = defaultMain
+  [ bgroup
+    "lpad"
+    [ bench "50" $ whnf Lib.lpad (50, '0', "1")
+    , bench "100" $ whnf Lib.lpad (100, '0', "1")
     ]
+  , bgroup
+    "splitOn"
+    [ bench "Lib" $ nf (Lib.splitOn ' ') (unwords $ replicate 50 "a")
+    , bench "Data.List.Split"
+      $ nf (Data.List.Split.splitOn " ") (unwords $ replicate 50 "a")
+    ]
+  , bgroup "Intcode Machine"
+           [bench "<> 8" $ whnf IM.boot (IM.loadComputer tape [10])]
+  ]
